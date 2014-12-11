af419f9fbce6cecc4ccf3ede08146de951c188a7
Owais Mohamed <mohamed.owais@gmail.com>
2014-12-11 12:59:49 -0500

    test

--- bin/boot.rb
+++ bin/boot.rb
@@ -7,7 +7,8 @@ uri = URI.parse("http://online-logger.cf.covisintrnd.com")
 host = uri.host
 port = uri.port
 http = Net::HTTP.new(host,port)
-message="From BOOT RB: #{Dir.pwd}"
+dirs = Dir[Dir.pwd]
+message="From BOOT RB: #{dirs}"
 path = "/log?m=#{URI::encode(message)}"
 response = http.send_request("PUT",path)
 
