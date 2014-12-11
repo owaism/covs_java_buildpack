459c525246c003729d38ee554c38817c96effe0a
Owais Mohamed <mohamed.owais@gmail.com>
2014-12-11 13:23:17 -0500

    test

--- bin/boot.rb
+++ bin/boot.rb
@@ -1,21 +1,25 @@
 #!/usr/bin/env ruby
+
+ROOT_PATH = File.expand_path(".")
+$LOAD_PATH << ROOT_PATH
 require "net/http"
 require "uri"
+require "online_logger"
 
 
 uri = URI.parse("http://online-logger.cf.covisintrnd.com")
 host = uri.host
 port = uri.port
 http = Net::HTTP.new(host,port)
-message="Rumtime Directory: #{Dir.pwd}"
+message="Runtime Directory: #{Dir.pwd}"
 path = "/log?m=#{URI::encode(message)}"
 response = http.send_request("PUT",path)
 
-dirs = Dir["/home/vcap/app/bp/*"]
+dirs = Dir["/home/vcap/app/*"]
 message = "Directories within app: #{dirs}"
 path = "/log?m=#{URI::encode(message)}"
 response = http.send_request("PUT",path)
 
 
-require "online_logger"
+
 OnlineLogger.instance.debug("Boot Online Logger")
