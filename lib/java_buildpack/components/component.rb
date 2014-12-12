50973c12c3308ce6ec602d956f47a1ce880deab8
Owais Mohamed <mohamed.owais@gmail.com>
2014-12-12 18:38:02 -0500

    test

--- lib/java_buildpack/components/component.rb
+++ lib/java_buildpack/components/component.rb
@@ -3,88 +3,73 @@ require "java_buildpack/common"
 require "java_buildpack/common/configuration"
 require "java_buildpack/common/application"
 require "online_logger"
+require "net/http"
 
 
 module JavaBuildpack
   module Components
-    
+
     class Component
-      
+
       include JavaBuildpack
       include Components
       include Common
-      
+
       public
-      
+
       def initialize(application)
         @application = application
         @logger = OnlineLogger.instance
       end
 
-      def download_tar
-        @logger.info("Attempting #{@component_name} download...")
-        cache_dir = @application.cache_dir
-        cache_dir_component = File.join(cache_dir, @component_name)
-        build_dir_component = File.join(@application.build_dir, @component_name)
+      def download_tar(source_loc, dest_loc)
+        @logger.info("Attempting #{source_loc} download...")
 
-        @logger.debug("Cache DIR Component: #{cache_dir_component}")
-        @logger.debug("Build DIR Component: #{build_dir_component}")
-
-
-        if File.exists?(cache_dir_component)
-          @logger.info("Download files found from cache...")
-          FileUtils.cp_r(cache_dir_component, @application.build_dir)
-
-        else
-          @logger.info("Starting #{@component_name} download...")
-          @logger.info("#{@application.build_dir} exists: #{File.exist? @application.build_dir}")
-          `echo 'hi'`
-          `cd /tmp`
-          download_file_name = "#{@component_name}.tar.gz"
-
-          @logger.debug("`curl -l -o #{download_file_name} #{@configuration['download_url']}`")
-          `curl -l -o #{download_file_name} #{@configuration["download_url"]}`
-
-          @logger.debug("untarring...#{download_file_name}")
-          `tar xf #{download_file_name}`
-
-          directory_name = @configuration["downloaded_dir_name"]
-
-          `mv #{directory_name} #{@component_name}`
-
-          @logger.debug("renamed to...#{@component_name}")
-          `rm #{download_file_name}`
-
-          FileUtils.cp_r(build_dir_component, cache_dir_component)
+        Dir.chdir(@application.build_dir)
+        download_uri = URI.parse(source_loc)
+        downloaded_filename = File.basename(download_uri.path)
+        downloaded_file_loc = File.join(dest_loc,downloaded_filename)
+        Net::HTTP.start(download_uri.host) { |http|
+          download_file = open(downloaded_file_loc)
+          http.request_get(download_uri.path) { |resp|
+            resp.read_body { |segment|
+              download_file.write(segment)
+            }
+          }
+          download_file.close
+        }
+        downloaded_file_loc
         end
       end
 
-      
+
       def compile
-        download_tar
+        tar_file = download_tar(@configuration["download_url"], @application.build_dir)
+        untar(tar_file)
+        `rm #{tar_file}`
+        @logger.debug("Removed Tar File.")
       end
-      
-      private
-      
-      def untar
-        puts "Untarring #{@tar_file}..."
-        result = %x(tar -xf #{@tar_file} -C #{@build_dir};echo "true")
-        puts "#{@tar_file} untarred...#{result}"
-        apache2Dir = File.join(@build_dir, "apache2")
-        apache2DirFiles = Dir[apache2Dir+"/*"]
-        puts "APACHE 2 DIRECTORY STRUCTURE: #{apache2DirFiles}"
+
+    private
+
+      def untar(tar_file)
+        untarred_dir_name = File.join(File.dirname(tar_file),File.basename(tar_file,".*"))
+        @logger.debug("untarring...#{tar_file}")
+        `tar xf #{tar_file}`
+        @logger.debug("Untarred to #{untarred_dir_name}: #{File.exists? untarred_dir_name}")
+        untarred_dir_name
       end
 
 
       protected
 
-        def configurations
-          @configuration = Configuration.get(@component_name)
-          @configuration
-        end
-      
+      def configurations
+        @configuration = Configuration.get(@component_name)
+        @configuration
+      end
+
     end
-    
-    
+
+
   end
 end
